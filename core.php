abf8e4a3ce9aa68db74b74050255b7ed295ef11a
viennacompany <info@viennad1.ir>
2023-12-06 15:28:47 +0330

    new p Akam

---
+++ core.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Plugin Name: افزونه چت و تماس با ما آکام
+Description: این افزونه به شما این امکان را می‌دهد تا ارتباطی آسان تر با کاربران خود داشته باشید
+Version: 1.0.0
+Author: پرتو گستر ویانا
+Author URI: https://viennaco.ir/
+Plugin URI: https://github.com/lordwebiran/Gold-Price
+*/
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Core
+{
+    private static $_instance = null;
+
+    const MINIMUM_PHP_VERSION = "7.4";
+    const Plugin_Name = "آکام";
+
+    public static function instance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public function __construct()
+    {
+        if (version_compare(PHP_VERSION, self::MINIMUM_PHP_VERSION, '<')) {
+            add_action('admin_notices', [$this, 'admin_notice']);
+            return;
+        }
+        $this->constans();
+        $this->init();
+    }
+
+    public function constans()
+    {
+        define('G_BASE_FILE', __FILE__);
+        define('G_PLUGIN_DIR', plugin_dir_path(__FILE__));
+        define('G_PLUGIN_URL', plugin_dir_url(__FILE__));
+        define('G_ASSETS_URL', G_PLUGIN_URL . 'assets/');
+        define('G_includes_DIR', G_PLUGIN_DIR . 'includes/');
+        define('G_admin_DIR', G_includes_DIR . 'admin/');
+        define('G_admin_views_DIR', G_admin_DIR . 'views/');
+        define('G_ferant_DIR', G_includes_DIR . 'ferant/');
+    }
+    public function init()
+    {
+        require_once G_PLUGIN_DIR . 'vendor/autoload.php';
+        require_once G_PLUGIN_DIR . 'inc/admin/codestar/codestar-framework.php';
+
+        register_activation_hook(G_BASE_FILE, [$this, 'active']);
+        register_deactivation_hook(G_BASE_FILE, [$this, 'deactive']);
+
+        //if (is_admin()) {
+        //    new VC_Menu();
+        //}
+
+        //new G_Assets();
+
+        //function sale()
+        //{
+        //    $manager = new VC_sale_m();
+        //    $manager->page();
+        //}
+        //add_shortcode('sale', 'sale');
+    }
+    public function active()
+    {
+        //VC_DB::create_table();
+    }
+
+    public function deactive()
+    {
+    }
+
+    public function admin_notice()
+    {
+?>
+        <div class="notice notice-warning is-dismissible">
+            <p><?php echo esc_html('افزونه <b>' . self::Plugin_Name . '</b> برای اجرای صحیح نیاز به نسخه <b>' . self::MINIMUM_PHP_VERSION . ' PHP</b> به بالا دارد، لطفا نسخه PHP خود را ارتقا دهید'); ?></p>
+        </div>
+<?php
+    }
+}
+
+Core::instance();
